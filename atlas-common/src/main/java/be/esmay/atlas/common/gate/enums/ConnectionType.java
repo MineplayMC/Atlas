580239f263d0377816facbc2eb7a5ef65836c642
Esmaybe <esmaybe@craftville.net>
2025-10-01 23:22:57 +0200

    Updated enum

--- atlas-common/src/main/java/be/esmay/atlas/common/gate/enums/ConnectionType.java
+++ atlas-common/src/main/java/be/esmay/atlas/common/gate/enums/ConnectionType.java
@@ -2,6 +2,6 @@ package be.esmay.atlas.common.gate.enums;
 
 public enum ConnectionType {
 
-    STATIC, RANDOM, LEAST_PLAYERS, OPTIMAL_SERVER, OPTIMAL_SERVER_OR_LOBBY, LAST_CONNECTED;
+    STATIC, RANDOM, LEAST_PLAYERS, LAST_CONNECTED;
 
 }
